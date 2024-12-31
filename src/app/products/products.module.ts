af6953fe31c12c1650ebc9e5f248bf74f3cc6e79
HagerMagdy2 <hager9671@gmail.com>
2024-12-31 15:16:17 +0200

    try

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule ,NO_ERRORS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllProductsComponent } from './components/all-products/all-products.component';
 
 
-
-
 @NgModule({
   declarations: [AllProductsComponent],
-  imports: [
-    CommonModule
-  ],
-  
-  
+  schemas: [NO_ERRORS_SCHEMA],
+  exports: [AllProductsComponent],
+  imports: [CommonModule]
 })
 export class ProductsModule { }
