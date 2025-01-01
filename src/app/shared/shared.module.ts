3b122f02a99d627fd7e1ecc54c70aa56c9f9609a
HagerMagdy2 <hager9671@gmail.com>
2025-01-01 23:52:55 +0200

    products by category name

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { SpinnerComponent } from './components/spinner/spinner.component';
@@ -11,11 +11,10 @@ import { SpinnerComponent } from './components/spinner/spinner.component';
 @NgModule({
   declarations: [HeaderComponent, SpinnerComponent],
   imports: [
-    
     RouterModule,
     HttpClientModule,
     FormsModule,
-    CommonModule,
+    CommonModule,//didn't work
   ],
   exports: [HeaderComponent,SpinnerComponent]
 })
